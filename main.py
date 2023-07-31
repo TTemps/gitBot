3789b392fcc8d5c2bec119a91c10d463e14ae815
TTemps <tristan.morel21@gmail.com>
2023-07-31 13:19:11 +0200

    ajout des fêtes

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from git import Repo
 import os
 import datetime
 
+# Liste des fêtes avec les messages correspondants
+holidays = [
+    (datetime.date(datetime.date.today().year, 1, 1), " - Bonne année! 🎉"),
+    (datetime.date(datetime.date.today().year, 5, 1), " - Joyeuse fête du travail! 🌷"),
+    (datetime.date(datetime.date.today().year, 7, 14), " - Bonne fête nationale! 🇫🇷"),
+    (
+        datetime.date(datetime.date.today().year, 8, 15),
+        " - Joyeuse fête de l'Assomption! 🌺",
+    ),
+    (
+        datetime.date(datetime.date.today().year, 11, 1),
+        " - Joyeuse fête de la Toussaint! 🍂",
+    ),
+    (
+        datetime.date(datetime.date.today().year, 11, 11),
+        " - Bonne fête de l'Armistice! 🕊️",
+    ),
+    (datetime.date(datetime.date.today().year, 12, 25), " - Joyeux Noël! 🎄"),
+]
+
 
 def create_commit(commit_message):
     repo = Repo(".")
@@ -40,6 +60,12 @@ def get_number_days_year():
 def main():
     days_left = get_days_until_new_year()
     commit_message = f"Commit {get_number_days_year()-days_left+1}/{get_number_days_year()} : {days_left} days left"
+
+    # Vérifier si c'est l'une des fêtes spéciales
+    for holiday_date, holiday_message in holidays:
+        if datetime.date.today() == holiday_date:
+            commit_message += holiday_message
+
     print(commit_message)
     create_commit(commit_message)
 

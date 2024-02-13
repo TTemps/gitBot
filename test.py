06a6c0becc06ff7489b94884359fb0bb2321ce86
TTemps <tristan.morel21@gmail.com>
2024-02-13 14:47:01 +0100

    cc

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ url = "https://api.open-meteo.com/v1/forecast"
 params = {
     "latitude": 49.4431,
     "longitude": 1.0993,
+    "current": ["temperature_2m", "is_day", "weather_code"],
     "daily": "sunrise,sunset",
     "timezone": "auto",
 }
@@ -19,15 +20,21 @@ if 'daily' in data:
     dates = data['daily']['time']
     sunrise = data['daily']['sunrise']
     sunset = data['daily']['sunset']
+    temperature = data['current']['temperature_2m']
+    is_day = data['current']['is_day']
+    weather_code = data['current']['weather_code']
     
     # Préparation du DataFrame
     daily_data = {
         "date": dates,
         "sunrise": sunrise,
-        "sunset": sunset
+        "sunset": sunset,
+        "temperature": temperature,
+        "is_day": is_day,
+        "weather_code": weather_code
     }
     
-    daily_dataframe = pd.DataFrame(daily_data)
-    print(daily_dataframe)
+    #daily_dataframe = pd.DataFrame(daily_data)
+    print(daily_data)
 else:
     print("Données journalières non trouvées dans la réponse de l'API.")

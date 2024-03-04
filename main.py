1760900703ae78f1c5312901cf1eb2d3615e08cd
TTemps <tristan.morel@ttemps.com>
2024-03-04 22:03:49 +0100

    patch

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import logging
 import weather
 
 logging.basicConfig(filename='log_commit.txt', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s', encoding='utf-8')
+logging.basicConfig(filename='error_commit.txt', level=logging.ERROR, format='%(asctime)s:%(levelname)s:%(message)s', encoding='utf-8')
 
 # Liste des fêtes avec les messages correspondants
 holidays = [
@@ -31,14 +32,17 @@ holidays = [
 def create_commit(commit_message):
     try:
         try:
-            print("Commit message : ", commit_message)
+            logging.info(f"Commit message : {commit_message}")
         except Exception as e:
-            print(f"Erreur lors de l'affichage du message de commit : {e}")
+            logging.error(f"Erreur lors de l'écriture du message de commit : {e}")
         repo = Repo(".")
         index = repo.index
-        add_commit_message_to_file("message.txt", commit_message)
+        add_commit_message_to_file("message.txt", commit_message) # Ajouter le message au fichier
         index.add(["message.txt"])
-        
+        print("ccccc "+ commit_message)
+        #get the first list of the message.txt
+
+        breakpoint()
         index.commit(commit_message)
         origin = repo.remote("origin")
         origin.push()
@@ -47,58 +51,27 @@ def create_commit(commit_message):
         logging.error(f"Erreur lors du commit : {e}")
 
 
-def add_commit_message_to_file(filename, commit_message):
+def add_commit_message_to_file(filename, commit_message): # Ajouter le message complet au fichier
     with open(filename, "a", encoding='utf-8') as file:
         file.write(f"{commit_message}\n")
 
 
-def get_days_until_new_year():
+def get_days_until_new_year(): # Récupérer le nombre de jours restants jusqu'à la nouvelle année
     today = datetime.date.today()
     new_year = datetime.date(today.year + 1, 1, 1)
     days_left = (new_year - today).days
     return days_left
 
 
-def get_number_days_year():
+def get_number_days_year(): # Récupérer le nombre de jours dans l'année courante
     today = datetime.date.today()
     first_day = datetime.date(today.year + 1, 1, 1)
     last_day = datetime.date(today.year + 1, 12, 31)
     total_day = (last_day - first_day).days
     return total_day
 
-def get_last_commit_content(file_path, num_lines=8):
-    """
-    Récupère le contenu des dernières lignes d'un fichier correspondant à un commit.
-    
-    Args:
-        file_path (str): Chemin du fichier à lire.
-        num_lines (int): Nombre de lignes à lire depuis la fin du fichier.
-        
-    Returns:
-        str: Le contenu des dernières lignes du fichier.
-    """
-    try:
-        with open(file_path, "r", encoding="utf-8") as file:
-            # Lire toutes les lignes du fichier et récupérer les dernières 'num_lines' lignes
-            lines = file.readlines()[-num_lines:]
-            return ''.join(lines).strip()  # Concaténer et nettoyer les lignes
-    except Exception as e:
-        print(f"Erreur lors de la lecture du fichier {file_path}: {e}")
-        return None
-
-def is_file_content_identical(file_path, content, num_lines=8):
-    if not os.path.exists(".git"):
-        return False
-    repo = Repo(".")
-    last_commit_content = get_last_commit_content(file_path, num_lines)
-    print(last_commit_content)
-    print(content)
-    # Comparer le contenu prévu pour le commit avec le dernier contenu commité
-    return content.strip() == last_commit_content.strip()
-
-
-def initialize_message_file():
-    initialized = False
+def initialize_message_file(): # Initialiser le fichier message.txt si nécessaire
+    initialized = False 
     if not os.path.exists("message.txt"):
         logging.info("Création du fichier 'message.txt'.")
         with open("message.txt", "w", encoding='utf-8') as file:
@@ -113,18 +86,10 @@ def initialize_message_file():
     if initialized:
         create_commit("Initialisation du fichier 'message.txt'.")
         
-# def is_file_content_identical(file_path, content):
-#     if not os.path.exists(".git"):
-#         return False
-
-#     repo = Repo(".")
-#     last_commit = repo.head.commit
-#     last_line_message_txt = get_last_line_files(file_path)
-#     return content.strip() == last_line_message_txt.strip()
-
 def set_message():
     days_left = get_days_until_new_year()
-    message = f"Commit {get_number_days_year()-days_left+1}/{get_number_days_year()} : {days_left} jours restants"
+    commit_message = f"Commit {get_number_days_year()-days_left+1}/{get_number_days_year()} : {days_left} jours restants"
+    message = ""
     weather_data = weather.get_weather()
     message += f"""\nMétéo : 
         - Température {weather_data['temperature']} 
@@ -132,23 +97,37 @@ def set_message():
         - Temps :{weather_data['weather_code']}
         - Lever : {weather_data['sunrise']}
         - Coucher : {weather_data['sunset']}\n"""
-    return message
- 
+    return commit_message,message
+
+def get_last_commit_message(): # Récupérer le message du dernier commit
+    try:
+        repo = Repo(".")
+        last_commit = list(repo.iter_commits("main", max_count=1))[0]
+        last_commit_message = last_commit.message
+        return last_commit_message
+    except Exception as e:
+        logging.error(f"Erreur lors de la récupération du dernier commit : {e}")
+        return ""
+
+def is_commit_content_identical(last_commit_message, commit_message): # Vérifier si le contenu du fichier est identique au dernier commit
+    return last_commit_message.strip() == commit_message.strip()
+
 def main():
     
-    commit_message = set_message()
+    tmp = set_message()
+    commit_message = tmp[0]
+    message = tmp[1]
 
     # Vérifier si c'est l'une des fêtes spéciales
     for holiday_date, holiday_message in holidays:
         if datetime.date.today() == holiday_date:
             commit_message += holiday_message
             
-    logging.info("Contenu actuel : " + commit_message)
-    #last_line_message_txt = get_last_line_files("message.txt")
-    last_line_message_txt = get_last_commit_content("message.txt")
-    logging.info("Contenu du dernier commit : " + last_line_message_txt)
+    logging.info("Commit actuel : " + commit_message)
+    last_commit_message = get_last_commit_message()
+    logging.info("Message du dernier commit : " + last_commit_message)
     # Vérifier si le contenu du fichier est identique au dernier commit
-    if is_file_content_identical("message.txt", commit_message):
+    if is_commit_content_identical(last_commit_message, commit_message):
         logging.info("Aucun changement détecté, pas de commit effectué.")
     else:
         logging.info("Print avant commit : " + commit_message)
